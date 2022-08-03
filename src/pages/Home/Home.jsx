67eb8f7108e5b4ac7ab9eca22377f27e46923a58
ioumesh <umeshkumar@zingbus.com>
2022-08-04 00:07:11 +0530

    feat:api added and fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
   useEffect(() => {
     fetchPokemonList();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pokemonList]);
+  }, []);
   return (
     <div>
       <Topbar />
